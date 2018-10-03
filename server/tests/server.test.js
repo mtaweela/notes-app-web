daece7b0f135eb6f4bb26447fc18a2072a17d877
mtaweela <mohamed.taweela56@gmail.com>
2018-10-03 13:34:24 +0200

    add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,11 +2,10 @@ const expect = require("expect");
 const request = require("supertest");
 const { ObjectID } = require("mongodb");
 
-const {User} = require('./../models/user')
+const { User } = require("./../models/user");
 const { app } = require("./../server");
 const { Todo } = require("./../models/todos");
-const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
-
+const { todos, populateTodos, users, populateUsers } = require("./seed/seed");
 
 beforeEach(populateUsers);
 beforeEach(populateTodos);
@@ -14,7 +13,7 @@ beforeEach(populateTodos);
 describe("POST /todos", () => {
   it("should create a new todo", done => {
     var text = "test todo text";
-    var title = 'new title';
+    var title = "new title";
 
     request(app)
       .post("/api/todos")
@@ -176,77 +175,129 @@ describe("PUT /todos/:id", () => {
   });
 });
 
-
-describe('GET /users/me', () => {
-  it('should return user if authenticated', (done) => {
+describe("GET /users/me", () => {
+  it("should return user if authenticated", done => {
     request(app)
-      .get('/api/users/me')
-      .set('x-auth', users[0].tokens[0].token)
+      .get("/api/users/me")
+      .set("x-auth", users[0].tokens[0].token)
       .expect(200)
-      .expect((res) => {
+      .expect(res => {
         expect(res.body._id).toBe(users[0]._id.toHexString());
         expect(res.body.email).toBe(users[0].email);
       })
       .end(done);
   });
 
-  it('should return 401 if not authenticated', (done) => {
+  it("should return 401 if not authenticated", done => {
     request(app)
-      .get('/api/users/me')
+      .get("/api/users/me")
       .expect(401)
-      .expect((res) => {
+      .expect(res => {
         expect(res.body).toEqual({});
       })
       .end(done);
   });
 });
 
-describe('POST /users', () => {
-  it('should create a user', (done) => {
-    let email = 'one@gmail.com';
+describe("POST /users", () => {
+  it("should create a user", done => {
+    let email = "one@gmail.com";
     var password = "1234qwer";
 
     request(app)
-      .post('/api/users')
-      .send({email, password})
+      .post("/api/users")
+      .send({ email, password })
       .expect(200)
       .expect(res => {
-        expect(res.headers['x-auth']).toBeTruthy();
+        expect(res.headers["x-auth"]).toBeTruthy();
         expect(res.body._id).toBeTruthy();
         expect(res.body.email).toBe(email);
       })
-      .end((err) => {
+      .end(err => {
         if (err) {
           return done(err);
         }
 
-        User.findOne({email}).then(user => {
+        User.findOne({ email }).then(user => {
           expect(user).toBeTruthy();
           expect(user.password).not.toBe(password);
           done();
-        })
+        }).catch(err => done(err));
       });
   });
 
-  it('should return validation error if request is invalid', (done) => {
+  it("should return validation error if request is invalid", done => {
     request(app)
-      .post('/api/users')
+      .post("/api/users")
       .send({
-        email: 'mmm',
-        password: 'ddd'
+        email: "mmm",
+        password: "ddd"
       })
       .expect(400)
       .end(done);
   });
 
-  it('should not create user if email in use', (done) => {
+  it("should not create user if email in use", done => {
     request(app)
-      .post('/api/users')
+      .post("/api/users")
       .send({
         email: users[0].email,
-        password: '1234wqer'
+        password: "1234wqer"
       })
       .expect(400)
       .end(done);
+  });
+});
+
+describe("POST /users/login", () => {
+  it("should login user and return auth token", done => {
+    request(app)
+      .post("/api/users/login")
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.headers["x-auth"]).toBeTruthy();
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then(user => {
+          expect(user.tokens[0]).toMatchObject({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+
+          done();
+        }).catch(err => done(err));
+      });
+  });
+
+  it("should reject invalid token", done => {
+    request(app)
+      .post("/api/users/login")
+      .send({
+        email: users[1].email,
+        password: users[1].password + '1'
+      })
+      .expect(400)
+      .expect(res => {
+        expect(res.headers["x-auth"]).toBeFalsy();
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then(user => {
+          expect(user.tokens.lengh).toBe(undefined);
+
+          done();
+        }).catch(err => done(err));
+      });
   });
 });
